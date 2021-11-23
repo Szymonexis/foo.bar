681fd21a58e6b6a4d90c2172ec69ec2396ec8782
Szymonexis <szymonkaszuba3@wp.pl>
2021-11-23 03:36:42 +0100

    updating fuel.py

--- fuel.py
+++ fuel.py
@@ -33,8 +33,12 @@
 #     [0, 3, 2, 9, 14]
 
 import numpy as np
+from fractions import gcd
+from functools import reduce
 from copy import deepcopy
 from pprint import pprint
+
+from numpy.lib.histograms import _search_sorted_inclusive
 
 
 def transform_to_tuples(states):
@@ -72,30 +76,95 @@ def populate_absorbing_states(states):
     return states
 
 
+def get_lcm(states):
+    sums = []
+    for line in states:
+        sum = 0
+        for value in line:
+            sum += value
+        sums.append(sum)
+
+    absolute_multip = 1
+    for sum in sums:
+        absolute_multip = abs(absolute_multip * sum)
+    return absolute_multip // reduce(gcd, sums)
+
+
+def make_all_states_equal(states, lcm):
+    for x in range(len(states)):
+        sum = 0
+        for y in range(len(states[x])):
+            sum += states[x][y]
+        multiply = lcm // sum
+        for y in range(len(states[x])):
+            states[x][y] *= multiply
+    return states
+
+
+def check_if_square(states):
+    if len(states) != len(states[0]):
+        return False
+    return True
+
+
+def standard_form(states, lcm, states_original):
+    absorbing_states_indexes = []
+    for x in range(len(states_original)):
+        only_zeros = True
+        for y in range(len(states_original[x])):
+            if states_original[x][y] != 0:
+                only_zeros = False
+        absorbing_states_indexes.append(only_zeros)
+    
+    changes = [[(x, y) for y in range(len(states))] for x in range(len(states))]
+    absorbing_states = []
+    nonabsorbing_states = []
+
+    for x in range(len(states)):
+        if absorbing_states_indexes[x]:
+            absorbing_states.append(states[x])
+        else:
+            nonabsorbing_states.append(states[x])
+    absorbing_states_indexes = [value for value in absorbing_states_indexes if value]
+    amount = len(absorbing_states_indexes)
+    
+    # TODO: set the matrix into a standard form
+
+
+
 def solution(states):
+    if not check_if_square(deepcopy(states)):
+        return None
+
+    states_original = deepcopy(states)
+    states = populate_absorbing_states(states)
+    lcm = get_lcm(states)
+    states = make_all_states_equal(states, lcm)
+    standard_form(states, lcm, states_original)
+
+
     pass
         
 
 
 lst = [
-    [1, 2],
-    [0, 0]
+    [5, 2, 0],
+    [0, 0, 0],
+    [2, 0, 0]
 ]
 
-lst_0 = [
-    [(1, 3), (2, 3)],
-    [(0, 1), (1, 1)]
-]
+solution(lst)
 
-pprint(transform_to_tuples(deepcopy(lst)))
 
-lst = populate_absorbing_states(lst)
+# pprint(transform_to_tuples(deepcopy(lst)))
 
-arr = np.empty((len(lst), len(lst)), dtype=np.int)
-arr[:] = lst
-pprint(arr)
+# lst = populate_absorbing_states(lst)
 
-pprint(np.linalg.inv(arr))
+# arr = np.empty((len(lst), len(lst)), dtype=np.int)
+# arr[:] = lst
+# pprint(arr)
+
+# pprint(np.linalg.inv(arr))
 # https://youtu.be/qhnFHnLkrfA?t=802
 
 
